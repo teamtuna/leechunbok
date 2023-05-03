0842badc17a199b586b17c43812c0d00d73682da
babosamo <babosamo@gmail.com>
2023-05-03 23:29:13 +0900

    add HomeScreen

--- app/src/main/java/dev/tuna/leechunbok/MainActivity.kt
+++ app/src/main/java/dev/tuna/leechunbok/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.tuna.leechunbok.ui.theme.LeechunbokTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +16,9 @@ class MainActivity : ComponentActivity() {
             LeechunbokTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LeechunbokTheme {
-        Greeting("Android")
     }
 }
