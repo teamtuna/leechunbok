79a84696aa59313e4232a4fe2ed8e42ac0ecc950
juyoung.lee <juyoung.lee@birdview.kr>
2023-05-08 23:44:56 +0900

    add five button in home screen

--- app/src/main/java/dev/tuna/leechunbok/HomeScreen.kt
+++ app/src/main/java/dev/tuna/leechunbok/HomeScreen.kt
@@ -1,30 +1,64 @@
 package dev.tuna.leechunbok
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.tuna.leechunbok.ui.theme.LeechunbokTheme
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreen() {
-    Greeting("Android")
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LeechunbokTheme {
-        Greeting("Android")
+    MemberButtons(members = TunaMember.values()) { member ->
+        when (member) {
+            TunaMember.EAST_JUNG -> {}
+            TunaMember.NOAH -> {}
+            TunaMember.JUYOUNG -> {}
+            TunaMember.ROBIN -> {}
+            TunaMember.KADE -> {}
+        }
     }
+}
+
+@Composable
+private fun MemberButtons(
+    modifier: Modifier = Modifier,
+    members: Array<TunaMember>,
+    onClick: ((TunaMember) -> Unit)? = null
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        members.forEach { member ->
+            Button(
+                onClick = { onClick?.invoke(member) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = member.koreanName)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun NameButtonsPreview() {
+    LeechunbokTheme {
+        MemberButtons(members = TunaMember.values())
+    }
+}
+
+enum class TunaMember(val koreanName: String) {
+    EAST_JUNG("정동진"),
+    NOAH("김정우"),
+    JUYOUNG("이주영"),
+    ROBIN("최선일"),
+    KADE("김지훈")
 }
