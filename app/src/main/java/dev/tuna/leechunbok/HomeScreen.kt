fa2ec328fca560a800e646d3f97400ea8bc551ed
karrel <karrel84@naver.com>
2023-05-10 23:29:32 +0900

    add five button in home screen (#21)
    * add five button in home screen
    
    * Preview 네이밍 변경
    
    * MemberButtons 의 인자인 onClick 을 notnullable 로 수정
    
    * 하드코딩된 string 을 strings.xml 로 옮깁니다.
    
    ---------
    
    Co-authored-by: juyoung.lee <juyoung.lee@birdview.kr>

--- app/src/main/java/dev/tuna/leechunbok/HomeScreen.kt
+++ app/src/main/java/dev/tuna/leechunbok/HomeScreen.kt
@@ -1,30 +1,67 @@
 package dev.tuna.leechunbok
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.tuna.leechunbok.ui.theme.LeechunbokTheme
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreen() {
-    Greeting("Android")
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LeechunbokTheme {
-        Greeting("Android")
+    MemberButtons(members = TunaMember.values()) { member ->
+        when (member) {
+            TunaMember.EAST_JUNG -> {}
+            TunaMember.NOAH -> {}
+            TunaMember.JUYOUNG -> {}
+            TunaMember.ROBIN -> {}
+            TunaMember.KADE -> {}
+        }
     }
+}
+
+@Composable
+private fun MemberButtons(
+    modifier: Modifier = Modifier,
+    members: Array<TunaMember>,
+    onClick: (TunaMember) -> Unit = { }
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        members.forEach { member ->
+            Button(
+                onClick = { onClick(member) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                val userName = stringResource(id = member.nameResource)
+                Text(text = userName)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MemberButtonsPreview() {
+    LeechunbokTheme {
+        MemberButtons(members = TunaMember.values())
+    }
+}
+
+enum class TunaMember(@StringRes val nameResource: Int) {
+    EAST_JUNG(R.string.name_east_jung),
+    NOAH(R.string.name_noah),
+    JUYOUNG(R.string.name_whale_shark),
+    ROBIN(R.string.name_robin),
+    KADE(R.string.name_kade)
 }
