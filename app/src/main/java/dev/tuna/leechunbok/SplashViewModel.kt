46c5c3967c44efecd4cab6a7eb0ed9d5c2d471c7
Ji Hoon Kim (Kade) <rlawlgns077@gmail.com>
2023-05-10 23:51:32 +0900

    Update app/src/main/java/dev/tuna/leechunbok/SplashViewModel.kt
    반영 완료
    
    Co-authored-by: Robin, Choi <haveahopes@gmail.com>

--- app/src/main/java/dev/tuna/leechunbok/SplashViewModel.kt
+++ app/src/main/java/dev/tuna/leechunbok/SplashViewModel.kt
@@ -14,7 +14,7 @@ class SplashViewModel: ViewModel() {
 
     init {
         viewModelScope.launch {
-            delay(2000)
+            delay(500)
             _splashLoadingState.value = false
         }
     }
