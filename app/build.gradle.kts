2f0eb6f108afd61aa198e62df26e86319eabc4fe
kade <kade@daangn.com>
2023-05-10 23:41:56 +0900

    add splash screen!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,11 @@ dependencies {
     implementation(project(":robin:app"))
     implementation(project(":whaleshark:app"))
     implementation(project(":noah:app"))
-implementation(project(":kade:app"))
+    implementation(project(":kade:app"))
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.5.1")
+
+    // splash screen
+    implementation ("androidx.core:core-splashscreen:1.0.0-beta02")
 }
--- app/src/main/java/dev/tuna/leechunbok/MainActivity.kt
+++ app/src/main/java/dev/tuna/leechunbok/MainActivity.kt
@@ -3,15 +3,22 @@ package dev.tuna.leechunbok
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.tuna.leechunbok.ui.theme.LeechunbokTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { splashViewModel.splashLoadingState.value }
         setContent {
             LeechunbokTheme {
                 // A surface container using the 'background' color from the theme
