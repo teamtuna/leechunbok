ca0efe17f5254b3251562edf36494989072ad5e5
Ji Hoon Kim (Kade) <rlawlgns077@gmail.com>
2023-05-11 00:18:25 +0900

    13 - add splash screen! (#28)
    * add splash screen!
    
    * Update app/src/main/java/dev/tuna/leechunbok/SplashViewModel.kt
    
    반영 완료
    
    Co-authored-by: Robin, Choi <haveahopes@gmail.com>
    
    ---------
    
    Co-authored-by: Robin, Choi <haveahopes@gmail.com>

---
+++ app/src/main/java/dev/tuna/leechunbok/SplashViewModel.kt
@@ -0,0 +1,21 @@
+package dev.tuna.leechunbok
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SplashViewModel: ViewModel() {
+
+    private val _splashLoadingState = MutableStateFlow(true)
+    val splashLoadingState = _splashLoadingState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(500)
+            _splashLoadingState.value = false
+        }
+    }
+}
