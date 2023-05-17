56d140af92a92efdb5bacf7416efc0b8a6630ebe
karrel <karrel84@naver.com>
2023-05-17 23:15:30 +0900

    WhaleSharkScreen 생성 (#34)
    Co-authored-by: juyoung.lee <juyoung.lee@birdview.kr>

---
+++ whaleshark/app/src/main/kotlin/dev/tuna/leechunbok/WhaleSharkScreen.kt
@@ -0,0 +1,11 @@
+package dev.tuna.leechunbok
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun WhaleSharkScreen() {
+    Text(text = "Hello ~!")
+}
