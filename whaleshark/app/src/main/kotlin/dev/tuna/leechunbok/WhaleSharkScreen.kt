d76d3f1e895de711d7564606e90663c320ae07fc
karrel <karrel84@naver.com>
2023-05-17 23:32:26 +0900

    44 - WhaleSharkScreen 에 LeechunbokTheme 추가 (#43)
    WhaleSharkScreen 에 LeechunbokTheme 추가
    
    Co-authored-by: juyoung.lee <juyoung.lee@birdview.kr>

--- whaleshark/app/src/main/kotlin/dev/tuna/leechunbok/WhaleSharkScreen.kt
+++ whaleshark/app/src/main/kotlin/dev/tuna/leechunbok/WhaleSharkScreen.kt
@@ -3,9 +3,12 @@ package dev.tuna.leechunbok
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dev.tuna.leechunbok.resource.LeechunbokTheme
 
 @Preview(showBackground = true)
 @Composable
 fun WhaleSharkScreen() {
-    Text(text = "Hello ~!")
+    LeechunbokTheme {
+        Text(text = "Hello ~!")
+    }
 }
