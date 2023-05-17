4336db70188625c6da68478156148863820cf792
karrel <karrel84@naver.com>
2023-05-18 00:00:58 +0900

    고래 상어 스크린에 더미 그리드 생성 (#50)
    Co-authored-by: juyoung.lee <juyoung.lee@birdview.kr>

--- whaleshark/app/src/main/kotlin/dev/tuna/leechunbok/WhaleSharkScreen.kt
+++ whaleshark/app/src/main/kotlin/dev/tuna/leechunbok/WhaleSharkScreen.kt
@@ -1,14 +1,45 @@
 package dev.tuna.leechunbok
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.tuna.leechunbok.resource.LeechunbokTheme
 
 @Preview(showBackground = true)
 @Composable
 fun WhaleSharkScreen() {
     LeechunbokTheme {
-        Text(text = "Hello ~!")
+        Surface(Modifier.fillMaxSize()) {
+            GridImages()
+        }
+    }
+}
+
+@Composable
+private fun GridImages() {
+    val gridItems = (1..20).toList()
+    LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+        items(gridItems) { item ->
+            GridItem(item)
+        }
+    }
+}
+
+@Composable
+fun GridItem(item: Int) {
+    Card {
+        Text(
+            text = "Item $item",
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
