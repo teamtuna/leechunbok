8af183fb14953bf676f3aadcc1e22d28eab0fc41
kade <kade@daangn.com>
2023-05-17 23:36:03 +0900

    리소스 모듈 추가

--- kade/app/build.gradle.kts
+++ kade/app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":common:resource"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
