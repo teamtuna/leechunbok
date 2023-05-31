521f7d0da62516b5eebd53ea13730897e5dea509
Robin <haveahopes@gmail.com>
2023-06-01 02:21:49 +0900

    fix - review내용 반영

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.3.1" apply false
+    alias(libs.plugins.android.application) apply false
+
     id("com.android.library") version "7.3.1" apply false
     //https://developer.android.com/jetpack/androidx/releases/compose-kotlin
     kotlin("android") version "1.8.10" apply false
@@ -8,7 +9,7 @@ plugins {
     id("androidx.navigation.safeargs.kotlin") version "2.5.1" apply false
 
     //https://dagger.dev/hilt/gradle-setup.html
-    id("com.google.dagger.hilt.android") version "2.45" apply false
+    alias(libs.plugins.hilt) apply false
 
     //https://github.com/mannodermaus/android-junit5
     id("de.mannodermaus.android-junit5") version "1.8.2.1" apply false
