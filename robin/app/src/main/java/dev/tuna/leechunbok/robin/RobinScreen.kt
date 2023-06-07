32c40b6ce9fdc8007744b2f00b30f2dcdde77275
Robin <haveahopes@gmail.com>
2023-06-08 00:09:43 +0900

    feat - tabLayout 추가

--- robin/app/src/main/java/dev/tuna/leechunbok/robin/RobinScreen.kt
+++ robin/app/src/main/java/dev/tuna/leechunbok/robin/RobinScreen.kt
@@ -1,16 +1,34 @@
 package dev.tuna.leechunbok.robin
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import dev.tuna.leechunbok.resource.LeechunbokTheme
 
@@ -35,8 +53,68 @@ fun RobinScreen() {
                         }
                     }
                 )
-            }) {
+            }) { padding ->
+            Column(Modifier.padding(padding)) {
+                RobinComponentTabRow()
+            }
+        }
+    }
+}
 
+@Composable
+fun RobinComponentTabRow() {
+    var state by remember {
+        mutableStateOf(0)
+    }
+    TabRow(selectedTabIndex = state) {
+        RobinComponentTab(
+            title = "Tab1",
+            selected = state == 0,
+            onClick = {
+                state = 0
+            }
+        )
+        RobinComponentTab(
+            title = "Tab2",
+            selected = state == 1,
+            onClick = {
+                state = 1
+            }
+        )
+        RobinComponentTab(
+            title = "Tab3",
+            selected = state == 2,
+            onClick = {
+                state = 2
+            }
+        )
+    }
+}
+
+@Composable
+fun RobinComponentTab(title: String, selected: Boolean, onClick: () -> Unit) {
+    Tab(selected = selected, onClick = onClick) {
+        Column(
+            Modifier
+                .padding(10.dp)
+                .height(50.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                Modifier
+                    .size(10.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .background(
+                        color = if (selected) MaterialTheme.colorScheme.primary
+                        else MaterialTheme.colorScheme.background
+                    )
+            )
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
