101f04db4637ab412904ddc9a254de7e3656511d
Noah, Kim <babosamo@gmail.com>
2023-06-07 23:48:02 +0900

    Feature/#26 noah screen (#62)
    * add noah screen
    
    * connect screen

---
+++ noah/app/src/main/java/dev/tuna/leechunbok/noah/NoahScreen.kt
@@ -0,0 +1,9 @@
+package dev.tuna.leechunbok.noah
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NoahScreen() {
+    Text(text = "Noah Screen")
+}
