3b032af508f581ea0fb665c20fe80cd27be27c3b
Robin <haveahopes@gmail.com>
2023-06-14 23:58:05 +0900

    add - tablayout

--- robin/app/src/main/java/dev/tuna/leechunbok/robin/RobinScreen.kt
+++ robin/app/src/main/java/dev/tuna/leechunbok/robin/RobinScreen.kt
@@ -1,36 +1,33 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package dev.tuna.leechunbok.robin
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import dev.tuna.leechunbok.resource.LeechunbokTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
@@ -55,66 +52,37 @@ fun RobinScreen() {
                 )
             }) { padding ->
             Column(Modifier.padding(padding)) {
-                RobinComponentTabRow()
+                RobinComponentTabRow(listOf("Tab1", "Tab2", "Tab3"))
             }
         }
     }
 }
 
 @Composable
-fun RobinComponentTabRow() {
-    var state by remember {
-        mutableStateOf(0)
-    }
-    TabRow(selectedTabIndex = state) {
-        RobinComponentTab(
-            title = "Tab1",
-            selected = state == 0,
-            onClick = {
-                state = 0
-            }
-        )
-        RobinComponentTab(
-            title = "Tab2",
-            selected = state == 1,
-            onClick = {
-                state = 1
-            }
-        )
-        RobinComponentTab(
-            title = "Tab3",
-            selected = state == 2,
-            onClick = {
-                state = 2
-            }
-        )
-    }
-}
-
-@Composable
-fun RobinComponentTab(title: String, selected: Boolean, onClick: () -> Unit) {
-    Tab(selected = selected, onClick = onClick) {
-        Column(
-            Modifier
-                .padding(10.dp)
-                .height(50.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Box(
-                Modifier
-                    .size(10.dp)
-                    .align(Alignment.CenterHorizontally)
-                    .background(
-                        color = if (selected) MaterialTheme.colorScheme.primary
-                        else MaterialTheme.colorScheme.background
-                    )
-            )
-            Text(
-                text = title,
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
+fun RobinComponentTabRow(items: List<String>) {
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
+    TabRow(selectedTabIndex = pagerState.currentPage) {
+        items.forEachIndexed { index, title ->
+            Tab(
+                text = {
+                    Text(text = title)
+                },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    coroutineScope.launch {
+                        pagerState.scrollToPage(index)
+                    }
+                })
         }
+    }
+
+    HorizontalPager(pageCount = items.size, state = pagerState) { page ->
+        Text(
+            modifier = Modifier.wrapContentSize(),
+            text = page.toString(),
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp
+        )
     }
 }
