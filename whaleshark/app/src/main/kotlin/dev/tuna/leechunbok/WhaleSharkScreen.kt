8aade9d008b35ac29a24153b0eeefaf2639ef869
karrel <karrel84@naver.com>
2023-06-21 23:27:11 +0900

    38 dummy image loading (#68)
    * toml 에 coil 을 추가 합니다
    
    * compose.gradle 에서 coil 을 참조 합니다
    
    * 고래상어 화면에서 더미 이미지를 로드 합니다
    
    * toml 불필요한 주석 제거
    
    * app module 의 AndroidManifest.xml 에서 INTERNET permission 제거
    
    ---------
    
    Co-authored-by: juyoung.lee <juyoung.lee@birdview.kr>
    Co-authored-by: Robin, Choi <haveahopes@gmail.com>

--- whaleshark/app/src/main/kotlin/dev/tuna/leechunbok/WhaleSharkScreen.kt
+++ whaleshark/app/src/main/kotlin/dev/tuna/leechunbok/WhaleSharkScreen.kt
@@ -1,17 +1,18 @@
 package dev.tuna.leechunbok
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import dev.tuna.leechunbok.resource.LeechunbokTheme
 
 @Preview(showBackground = true)
@@ -36,10 +37,13 @@ private fun GridImages() {
 
 @Composable
 fun GridItem(item: Int) {
+    val imageUrl = "https://image.utoimage.com/preview/cp872722/2022/12/202212008462_206.jpg"
     Card {
-        Text(
-            text = "Item $item",
-            modifier = Modifier.padding(16.dp)
+
+        Image(
+            painter = rememberAsyncImagePainter(imageUrl),
+            contentDescription = "My Image",
+            modifier = Modifier.size(128.dp)
         )
     }
 }
